cefd61cb4a2bf75e32f4a7c484f662df94b514b5
Waldicéia de Fátima Barbalho <waldiceia.barbalho@aperam.com>
2023-04-06 11:03:39 -0300

    Aula 1 curso Pyhton Udemy

---
+++ Python_Basic/aula1.py
@@ -0,0 +1,19 @@
+"""
+DocString
+E escrever o que eu
+quiser
+asdfasdfd
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
+
+# Imprime um número
+# print(123) 
+""" Estou adorando Python """
+print('Agora sou dev.')
+#
